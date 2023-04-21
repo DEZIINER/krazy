d55a1a6030d7ba7fbeca2b427a916ba1ca96ccb6
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-04-21 09:42:01 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -491,7 +491,7 @@ async def auto_filter(client, msg, spoll=False):
         if 2 < len(message.text) < 100:
             search = message.text
             search_ = re.sub(
-                r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|,|dub(bed)|.|:|{|}|(|)|-|_|&|;|series|bluray|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|t(h)?emil|file|season(s)*|episode(s)*|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)",
+                r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|dub(bed)*|:|{|}|(|)|-|_|&|;|series|bluray|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|t(h)?emil|file|season(s)*|episode(s)*|und(o)*|full\smovie|any(one)|with\ssubtitle(s)?)",
                 "", search, flags=re.IGNORECASE)
             files, offset, total_results = await get_search_results(search_.lower(), offset=0, filter=True)
             if not files:
