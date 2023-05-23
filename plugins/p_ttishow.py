a70d4cbd6849a690f842b13908a1786d1cdfb2f5
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-05-23 19:46:50 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -42,7 +42,7 @@ async def save_group(bot, message):
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
-            text=f"<b>🔆 Thank You For Adding Me To Your Group 😇</b> \n\n⚜️<i><b>Make Me Admin Here</i></b> \n\n<i><b>🖲 Contact Support If You Have Any Doubts or Problems </i></b>",
+            text=f"<b>🔆 Thank You For Adding Me To Your Group 😇</b> \n\n⚜️<i><b>Make Me Admin & Send /connect to Use Me In Your Group</i></b> \n\n<i><b>🖲 Contact Support If You Have Any Doubts or Problems </i></b>",
             reply_markup=reply_markup)
     else:
         for u in message.new_chat_members:
