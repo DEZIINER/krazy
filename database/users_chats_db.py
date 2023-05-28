77757fe4eefd6b01f67bde74e1a7f8323eb5f68f
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-05-28 19:29:50 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -107,13 +107,8 @@ class Database:
     
     async def get_settings(self, id):
         default = {
-            'button': SINGLE_BUTTON,
-            'botpm': P_TTI_SHOW_OFF,
-            'file_secure': PROTECT_CONTENT,
-            'imdb': IMDB,
-            'spell_check': SPELL_CHECK_REPLY,
-            'welcome': MELCOW_NEW_USERS,
-            'template': IMDB_TEMPLATE
+            'imdb': IMDB
+            
         }
         chat = await self.grp.find_one({'id':int(id)})
         if chat:
