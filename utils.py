9eb9a8a0c10a47ae9743246e187b97b770a1dd76
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-06-01 16:17:30 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -382,8 +382,8 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://easysky.in/api'
-    params = {'api': '8abee50e122616d233cb904039c4fe594fe8574c',
+    url = f'URL_SHORTENR_WEBSITE'
+    params = {'api': 'URL_SHORTNER_WEBSITE_API',
               'url': link,
               }
 
