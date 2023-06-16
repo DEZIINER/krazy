0e7ac95e76782d4b2e938e39fb0292bc1dce1deb
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-06-16 16:36:44 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -28,7 +28,7 @@ SPELL_CHECK = {}
 async def give_filter(client, message):
     k = await global_filters(client, message)
     if k == False:
-    await auto_filter(client, message)
+        await auto_filter(client, message)
 
 @Client.on_callback_query(filters.regex(r"^next"))
 async def next_page(bot, query):
