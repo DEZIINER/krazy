58ceebfd574104f6ac4a973ab06a681c66829a06
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-06-19 19:57:40 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -129,7 +129,7 @@ async def advantage_spoll_choker(bot, query):
     if not movies:
         return await query.answer("🚫Link Expired, Request Again ♻", show_alert=True)
     movie = movies[(int(movie_))]
-    await query.answer("Checking in Database, Please Wait ♻️", show_alert=True)
+    await query.answer("Checking, Please Wait ♻️ \n\n [ ᴅᴏɴ'ᴛ sᴘᴀᴍ, ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ]", show_alert=True)
     k = await global_filters(bot, query.message, text=movie)
     if k == False:
         files, offset, total_results = await get_search_results(movie, offset=0, filter=True)
