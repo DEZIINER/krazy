f15a7eb43405aeff37f39abf4387bd55ccb96890
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-06-25 16:40:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -663,7 +663,7 @@ async def advantage_spell_chok(msg):
 async def global_filters(client, message, text=False):
     group_id = message.chat.id
     name = text or message.text
-    reply_id = message.reply_to_message.id if message.reply_to_message else message.id
+    reply_id = message.id
     keywords = await get_filters('filters')
     for keyword in reversed(sorted(keywords, key=len)):
         pattern = r"( |^|[^\w])" + re.escape(keyword) + r"( |$|[^\w])"
